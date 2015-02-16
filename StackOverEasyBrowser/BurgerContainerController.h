263bdb413e328948f44e09c266a1032cb4e2f0d7
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-16 13:24:53 -0800

    Adding BurgerContainerController as well as MainStoryBoard

---
+++ StackOverEasyBrowser/BurgerContainerController.h
@@ -0,0 +1,13 @@
+//
+//  BurgerContainerController.h
+//  StackOverEasyBrowser
+//
+//  Created by Rodrigo Carballo on 2/16/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BurgerContainerController : UIViewController
+
+@end
