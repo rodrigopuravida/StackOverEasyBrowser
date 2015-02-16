d049f5bf6df7d5d0b24d31f8182e92d2532b2df3
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-16 13:06:14 -0800

    Initial Commit

---
+++ StackOverEasyBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOverEasyBrowser
+//
+//  Created by Rodrigo Carballo on 2/16/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StackOverEasyBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StackOverEasyBrowser
+//
+//  Created by Rodrigo Carballo on 2/16/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
