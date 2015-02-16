abbdc167a77d4c7cd62de6200b57fab738adafd1
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-02-16 14:46:52 -0800

    Committing implementation of Segue to navigation controller where sliding panel is

--- StackOverEasyBrowser/BurgerContainerController.m
+++ StackOverEasyBrowser/BurgerContainerController.m
@@ -10,6 +10,14 @@
 
 @interface BurgerContainerController ()
 
+@property (strong,nonatomic) UINavigationController *searchVC;
+@property (strong,nonatomic) UIViewController *topViewController;
+@property (strong,nonatomic) UIButton *burgerButton;
+@property (strong,nonatomic) UITapGestureRecognizer *tapToClose;
+@property (strong,nonatomic) UIPanGestureRecognizer *slideRecognizer;
+
+
+
 @end
 
 @implementation BurgerContainerController
@@ -17,6 +25,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self addChildViewController:self.searchVC];
+    self.searchVC.view.frame = self.view.frame;
+    [self.view addSubview:self.searchVC.view];
+    [self.searchVC didMoveToParentViewController:self];
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(15, 15, 50, 50)];
+    [button setBackgroundImage:[UIImage imageNamed:@"burger"] forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(burgerButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.searchVC.view addSubview:button];
+    
+    self.burgerButton = button;
+    
+    self.tapToClose = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(closePanel)];
+    
+    self.slideRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(slidePanel:)];
+    [self.topViewController.view addGestureRecognizer:self.slideRecognizer];
+
+}
+
+-(void)slidePanel:(id)sender{
+    NSLog(@"Sliding Panel");
+    
+}
+
+-(void)closePanel{
+    NSLog(@"Closing Panel");
+}
+
+-(void) burgerButtonPressed {
+    NSLog(@"Burger ready to go");
+    
+}
+
+-(UINavigationController *)searchVC {
+    if (!_searchVC) {
+        _searchVC = [self.storyboard instantiateViewControllerWithIdentifier:@"SEARCH_VC"];
+    }
+    return _searchVC;
 }
 
 - (void)didReceiveMemoryWarning {
